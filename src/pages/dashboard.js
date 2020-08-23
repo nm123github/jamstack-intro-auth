cef544b1587ed16f498211ad4230984818919c28
Neville Mehta <neville.mehta@uber.com>
2020-08-23 08:10:52 -0700

    add login

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,21 +1,26 @@
 
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {navigate} from 'gatsby';
 import {Router} from '@reach/router';
+import IdentityModal from 'react-netlify-identity-widget';
 import Layout from '../components/layout';
 import Profile from '../components/profile';
 import RouteBase from '../components/route-base';
 import RouteSecret from '../components/route-secret';
 import RouteLogin from '../components/route-login';
 
+import 'react-netlify-identity-widget/styles.css';
+
 const Dashboard = ({location}) => {
+  const [isVisible, setVisibility] = useState(false);
+  const showModal = () => setVisibility(true);
 
   useEffect(() => {
     if (location.pathname.match(/^\/dashboard\/?$/)) {
       console.log('matched')
       navigate('/dashboard/login', {replace: true});
     }
-  }, []);
+  }, [location.pathname]);
 
   return (
     <Layout>
@@ -23,8 +28,12 @@ const Dashboard = ({location}) => {
       <Router>
         <RouteBase path="/dashboard/base" />
         <RouteSecret path="/dashboard/secret" />
-        <RouteLogin path="/dashboard/login" />
+        <RouteLogin showModal={showModal} path="/dashboard/login" />
       </Router>
+      <IdentityModal
+        showDialog={isVisible}
+        onCloseDialog={() => setVisibility(false)}
+      />
       <p>TODO: Create a dashboard</p>
     </Layout>
   )
